948f09684da0391ad72922bf41849f55ad33ae34
BiagioCaputo <scebiagio98@gmail.com>
2024-02-01 15:00:06 +0100

    added generics and reset css in app.vue and started the merge for the header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 export default {
+  name: 'Vite-comics',
+  components: { AppHeader }
 
 };
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
 <style lang="scss">
-@use './assets/scss/style.scss'
+@use './assets/scss/style.scss';
+
+/*Reset*/
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Gnereics */
+
+img {
+  max-width: 100%;
+}
+
+a {
+  text-decoration: none;
+}
 </style>
