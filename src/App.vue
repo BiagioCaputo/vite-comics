0d50ca2ba2cf80d87ad64d384a67a4677da65485
BiagioCaputo <scebiagio98@gmail.com>
2024-02-01 18:28:34 +0100

    creati elementi scss per lo style generale della pagina in modo da liberare App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,27 +20,4 @@ export default {
 
 <style lang="scss">
 @use './assets/scss/style.scss';
-
-/*Reset*/
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-/* Generics */
-
-img {
-  max-width: 100%;
-}
-
-a {
-  text-decoration: none;
-}
-
-/*Utilies*/
-.container {
-  max-width: 80%;
-  margin: 0 auto;
-}
 </style>
