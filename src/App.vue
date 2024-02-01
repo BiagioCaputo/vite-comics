d01c447be3f4c6c7646ce8b8e1f46ae478d8f76a
BiagioCaputo <scebiagio98@gmail.com>
2024-02-01 17:24:55 +0100

    Creating branch for the Bottom footer component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppUpperFooter from './components/AppUpperFooter.vue';
+import AppBottomFooter from './components/AppBottomFooter.vue';
 
 export default {
   name: 'Vite-comics',
-  components: { AppHeader, AppMain, AppUpperFooter }
+  components: { AppHeader, AppMain, AppUpperFooter, AppBottomFooter }
 
 };
 </script>
@@ -14,6 +15,7 @@ export default {
   <AppHeader />
   <AppMain />
   <AppUpperFooter />
+  <AppBottomFooter />
 </template>
 
 <style lang="scss">
