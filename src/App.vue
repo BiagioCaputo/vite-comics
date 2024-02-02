aa076bffd97298da511baad3ff612ba0ede21ddf
BiagioCaputo <scebiagio98@gmail.com>
2024-02-02 16:07:35 +0100

    added jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
 
 
 export default {
   name: 'Vite-comics',
-  components: { AppHeader, AppMain, AppFooter }
+  components: { AppHeader, AppMain, AppFooter, AppJumbotron }
 
 };
 </script>
 
 <template>
   <AppHeader />
+  <AppJumbotron />
   <AppMain />
   <AppFooter />
 </template>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppJumbotron'
+
+};
+</script>
+
+<template>
+    <div class="jumbotron">
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    height: 380px;
+    width: 100%;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
